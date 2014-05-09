f56f5c5341aadf078aed6ab207c226328d191677
Yongze Xu <yongzxu@Rory.local>
2014-05-09 00:17:00 -0600

    refine umark.sh

--- shell/.vim/umark.sh
+++ shell/.vim/umark.sh
@@ -7,22 +7,16 @@ function jump {
     cd -P "$MARKPATH/$1" 2>/dev/null || echo "No such mark: $1"
 }
 function mark { 
-    mkdir -p "$MARKPATH"; ln -s "$(pwd)" "$MARKPATH/$1"
+    mkdir -p "$MARKPATH" && ln -s "$(pwd)" "$MARKPATH/$1"
 }
 function unmark { 
-    if [ $# -eq 0 ]; then
-        cwd=`pwd`
-        iname="$MARKPATH/`basename $cwd`"
-    elif [ $# -eq 1 ]; then
-        iname="$MARKPATH/$1"
-    else
-        echo "Usage: unmark [name]"; exit 0
-    fi
+    [ $# -gt 1 ] && echo "Usage: unmark [name]" && return
+    [ $# -eq 0 ] && iname="$MARKPATH/`basename $(pwd)`"
+    [ $# -eq 1 ] && iname="$MARKPATH/$1"
 
-    [ -e "$iname" ] && rm -i "$iname"
+    [ -h "$iname" ] && rm -i "$iname"
 }
 function marks {
-    mkdir -p "$MARKPATH"
-    ls -l "$MARKPATH" | sed 's/  / /g' | sed 's/  / /g' | cut -d' ' -f9-
+    mkdir -p "$MARKPATH" && ls -l "$MARKPATH" | sed 's/  */ /g' | cut -d' ' -f9-
 }
 
