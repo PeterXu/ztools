3b0070c4b61887223263075718cea4595aeb23d3
Yongze Xu <yongzxu@YONGZXU-M-H04G.CISCO.COM>
2014-05-23 16:57:38 +0800

    fix tips for having some

--- shell/.vim/umark.sh
+++ shell/.vim/umark.sh
@@ -26,14 +26,23 @@ _jump() {
     pre=${COMP_WORDS[COMP_CWORD-1]}
     cur=${COMP_WORDS[COMP_CWORD]}
     opts=`mkdir -p "$MARKPATH" && ls --color=never "$MARKPATH" 2>/dev/null || ls "$MARKPATH" 2>/dev/null`
-    [ "$cur" = "" ] && tips="$opts" ||
+    [ "$pre" = "jump" ] && tips="$opts" ||
         for opt in $opts; do
             tip=${opt/$cur/} && [ "$tip" != "$opt" ] && tips+="$cur$tip"
         done
     COMPREPLY=($(compgen -W "$tips" -- "$cur"))
 }
 _unmark() {
-    _jump
+    local pre cur opts tips
+    COMPREPLY=()
+    pre=${COMP_WORDS[COMP_CWORD-1]}
+    cur=${COMP_WORDS[COMP_CWORD]}
+    opts=`mkdir -p "$MARKPATH" && ls --color=never "$MARKPATH" 2>/dev/null || ls "$MARKPATH" 2>/dev/null`
+    [ "$pre" = "unmark" ] && tips="$opts" ||
+        for opt in $opts; do
+            tip=${opt/$cur/} && [ "$tip" != "$opt" ] && tips+="$cur$tip"
+        done
+    COMPREPLY=($(compgen -W "$tips" -- "$cur"))
 }
 
 complete -F _jump jump
