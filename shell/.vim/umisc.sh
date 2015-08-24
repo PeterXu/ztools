6823bac344ca8ffa5d6b5bcaa7f46c25bafc0ee7
Yongze Xu <test@test>
2015-08-24 16:52:05 +0800

    enhance ps utils

--- shell/.vim/umisc.sh
+++ shell/.vim/umisc.sh
@@ -6,6 +6,7 @@ _make_vname() {
     local vname=`echo g_${1} | tr [:space:]- _`
     local vtrim=`echo $vname | tr -d [:alnum:]_`
     echo $vname | tr -d "$vtrim"
+    return 0
 }
 
 
@@ -17,6 +18,7 @@ map_usage() {
     [[ "$opt" =~ "set" ]] && echo "       map_set map_name map_key map_val"
     [[ "$opt" =~ "get" ]] && echo "       map_get map_name map_key"
     [[ "$opt" =~ "del" ]] && echo "       map_del map_name map_key"
+    return 0
 }
 
 map_set() {
@@ -68,13 +70,29 @@ complete -F _ssh ssh
 
 
 # ps -ef order by %mem/rsz
-ps_mem() {
+_ps_ef() {
+    local nth str opt
+    [ $# -lt 2 -o $# -gt 3 ] && return 1
+    nth=$1 && str="$2"
+    [ $# -eq 3 ] && opt=$3
+
     local opts
     if [ "$(uname)" = "Darwin" ]; then
         opts='uid,pid,ppid,stime,time,%cpu,%mem,vsz,rss,comm' 
     else
         opts='euid,pid,ppid,stime,time,%cpu,%mem,vsz,rsz,comm'
     fi
-    ps -eo $opts | sort -k 9 -n -s
+    ps -eo $opts | sort -k $nth -n -s $opt
+    if [[ ! "$opt" =~ "-r" ]]; then
+        ps -eo $opts | grep "$str"
+    fi
 }
+alias ps_mem="_ps_ef 9 %MEM"
+alias ps_cpu="_ps_ef 6 %CPU"
+alias ps_pid="_ps_ef 2 ' PID'"
+alias ps_time="_ps_ef 4 ' TIME'"
+alias ps_stime="_ps_ef 5 STIME"
+
+alias psr_stime="_ps_ef 5 STIME -r"
+alias psr_pid="_ps_ef 2 PID -r"
 
