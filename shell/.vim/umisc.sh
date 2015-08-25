6c937a03bb4173b6a1312fb0e1404963fe36b859
Yongze Xu <test@test>
2015-08-25 10:41:45 +0800

    add regex help of bash string compare/substr

--- shell/.vim/umisc.sh
+++ shell/.vim/umisc.sh
@@ -11,7 +11,7 @@ _make_vname() {
 
 
 ## for bash map
-map_usage() {
+map_help() {
     local opt
     [ $# -eq 0 ] && opt="set,get,del" || opt="$*"
     echo "usage:"
@@ -22,20 +22,20 @@ map_usage() {
 }
 
 map_set() {
-    [ $# -ne 3 ] && map_usage set && return 1
+    [ $# -ne 3 ] && map_help set && return 1
     local vname=`_make_vname "${1}_${2}"` || return 1
     eval "${vname}=\"${3}\""
 }
 
 map_get() {
-    [ $# -ne 2 ] && map_usage get && return 1
+    [ $# -ne 2 ] && map_help get && return 1
     local vname=`_make_vname "${1}_${2}"` || return 1
     declare -p ${vname} 2>/dev/null 1>&2 || return 1
     eval "echo \${${vname}}"
 }
 
 map_del() {
-    [ $# -ne 2 ] && map_usage del && return 1
+    [ $# -ne 2 ] && map_help del && return 1
     local vname=`_make_vname "${1}_${2}"` || return 1
     eval "unset ${vname}"
 }
@@ -69,7 +69,7 @@ _ssh() {
 complete -F _ssh ssh
 
 
-# ps -ef order by %mem/rsz
+## ps -ef order by %mem/rsz
 _ps_ef() {
     local nth str opt
     [ $# -lt 2 -o $# -gt 3 ] && return 1
@@ -95,4 +95,55 @@ alias ps-stime="_ps_ef 5 STIME"
 
 alias psr-stime="_ps_ef 5 STIME -r"
 alias psr-pid="_ps_ef 2 PID -r"
+
+
+_printx() {
+    if [ $# -eq 4 ]; then
+        printf "     %-24s%s %-16s%s\n"    "$1" "$2" "$3," "$4"
+    else
+        printf "$*\n"
+    fi
+}
+
+## string regex help
+regex_help() {
+    local str="abcde.abcde"
+    _printx     "usage:  e.g., str=\"${str}\""
+    _printx     "  0. strlen"
+    _printx     "expr \"\$str\" : \".*\""  "=>"  "$(expr "$str" : ".*")" "the length of string"
+    _printx     "\${#str}"        "=>"  "${#str}"       "the length of string"
+    _printx
+    _printx     "  1. substr"
+    _printx     "\${str:2}"       "=>"  "${str:2}"      "[2, the right end]"
+    _printx     "\${str:2:3}"     "=>"  "${str:2:3}"    "[2, 5(from the 2th+3)]"
+    _printx     "\${str:(-6):5}"  "=>"  "${str:(-6):5}" "[0, (-6)from the right end]"
+    _printx     "\${str#a*c}"     "=>"  "${str#a*c}"    "del the shortest from the leftmost <#>"
+    _printx     "\${str##a*c}"    "=>"  "${str##a*c}"   "del the longest from the leftmost <#>"
+    _printx     "\${str%c*e}"     "=>"  "${str%c*e}"    "del the shortest from the right end <%>"
+    _printx     "\${str%%c*e}"    "=>"  "${str%%c*e}"   "del the longest from the right end <%>"
+    _printx
+    _printx     "  2. replace"
+    _printx     "\${str/bcd/x}"   "=>"  "${str/bcd/x}"  "the first matched from the leftmost"
+    _printx     "\${str//bcd/x}"  "=>"  "${str//bcd/x}" "all matched string from the leftmost"
+    _printx     "\${str/#bcd/x}"  "=>"  "${str/#bcd/x}" "the leftmost: 'bcd'"
+    _printx     "\${str/#abc/x}"  "=>"  "${str/#abc/x}" "the leftmost: 'abc'"
+    _printx     "\${str/%bcd/x}"  "=>"  "${str/%bcd/x}" "the right end: 'bcd'"
+    _printx     "\${str/%cde/x}"  "=>"  "${str/%cde/x}" "the right end: 'cde'"
+    _printx
+    _printx     "  3. compare - logic true"
+    _printx     "     [[ "$str" == "a*" ]]"
+    _printx     "     [[ "$str" =~ .*\.abcde ]]"
+    _printx     "     [[ \"11\" < \"2\" ]]"
+    _printx
+
+    local fpath="/tmp/README.md" fname="README.md"
+    _printx     "  e.g. fpath=\"$fpath\" && fname=\"$fname\""
+    _printx     "echo \${fpath##*/}"      "=>"  "${fpath##*/}"  "get the file"
+    _printx     "echo \${fpath%/*}"       "=>"  "${fpath%/*}"   "get the path"
+    _printx     "echo \${fname%%.*}"      "=>"  "${fname%%.*}"  "get the file basename" 
+    _printx     "echo \${fname##*.}"      "=>"  "${fname##*.}"  "get the file extension name" 
+    _printx     "echo -e \${fname/./'\t'}"  "=>"  "$(echo -e ${fname/./'\t'})" "replace '.' with tab"
+    _printx
+}
+
 
