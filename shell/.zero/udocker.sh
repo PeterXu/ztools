5e854d46c32352fc3fa48ebb7308d33526be33e9
Peter <peter@uskee.org>
2015-11-05 10:20:24 +0800

    add dk-mingw for using in mingw

--- shell/.zero/udocker.sh
+++ shell/.zero/udocker.sh
@@ -149,8 +149,70 @@ _dk_pid() {
 _dk_enter() {
     _dk_ps_tips dk-enter "-f status=running"
 }
+_dk_mingw() {
+    [ "$kUname" != "MINGW" ] && return 1
+
+    local VM=default
+    local VBOXMANAGE
+    local DOCKER_MACHINE=docker-machine.exe
+    
+    which $DOCKER_MACHINE >/dev/null 2>&1 || return 1
+    
+    if [ ! -z "$VBOX_MSI_INSTALL_PATH" ]; then
+      VBOXMANAGE=${VBOX_MSI_INSTALL_PATH}VBoxManage.exe
+    else
+      VBOXMANAGE=${VBOX_INSTALL_PATH}VBoxManage.exe
+    fi
+
+    local BLUE='\033[1;34m'
+    local GREEN='\033[0;32m'
+    local NC='\033[0m'
+
+    if [ ! -f "${VBOXMANAGE}" ]; then
+      echo "Either VirtualBox or Docker Machine are not installed. Please re-run the Toolbox Installer and try again."
+      return 1
+    fi
+
+    "${VBOXMANAGE}" showvminfo $VM &> /dev/null
+    local VM_EXISTS_CODE=$?
+
+    set -e
+
+    if [ $VM_EXISTS_CODE -eq 1 ]; then
+      echo "Creating Machine $VM..."
+      $DOCKER_MACHINE rm -f $VM &> /dev/null || :
+      rm -rf ~/.docker/machine/machines/$VM
+      $DOCKER_MACHINE create -d virtualbox --virtualbox-memory 2048 $VM
+    else
+      echo "Machine $VM already exists in VirtualBox."
+    fi
+
+    echo "Starting machine $VM..."
+    $DOCKER_MACHINE start $VM
+
+    echo "Setting environment variables for machine $VM..."
+    eval "$($DOCKER_MACHINE env --shell=bash $VM)"
+
+    clear
+    cat << EOF
 
 
+                        ##         .
+                  ## ## ##        ==
+               ## ## ## ## ##    ===
+           /"""""""""""""""""\___/ ===
+      ~~~ {~~ ~~~~ ~~~ ~~~~ ~~~ ~ /  ===- ~~~
+           \______ o           __/
+             \    \         __/
+              \____\_______/
+
+EOF
+    echo -e "${BLUE}docker${NC} is configured to use the ${GREEN}$VM${NC} machine with IP ${GREEN}$($DOCKER_MACHINE ip $VM)${NC}."
+    echo "For help getting started, check out the docs at https://docs.docker.com."
+    echo "NOTE: When using interactive commands, prepend winpty."
+    echo "Examples: 'winpty docker run -it ...', 'winpty docker exec -it ...'."
+    echo
+}
 
 ### init docker
 __init_docker() {
@@ -170,6 +232,7 @@ __init_docker() {
     alias dk-ls="docker images"
     alias dk-pid="$kSudo docker inspect --format '{{.State.Pid}}'"
     alias dk-ip="$kSudo docker inspect --format '{{ .NetworkSettings.IPAddress }}'"
+    alias dk-mingw="_dk_mingw"
 
     complete -F _dk_rmi dk-rmi
     complete -F _dk_run dk-run
